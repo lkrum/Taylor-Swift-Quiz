6be91f35944e4723f53e36f13de0832564cf7c81
lkrum <leahkrumholz73@gmail.com>
2023-04-08 18:02:27 -0500

    added timer function

---
+++ script.js
@@ -0,0 +1,41 @@
+// WHEN I click the start button
+// THEN a timer starts and I am presented with a question
+// WHEN I answer a question
+// THEN I am presented with another question
+// WHEN I answer a question incorrectly
+// THEN time is subtracted from the clock
+// WHEN all questions are answered or the timer reaches 0
+// THEN the game is over
+// WHEN the game is over
+// THEN I can save my initials and score
+
+
+// variables
+var timeEl;
+var question;
+var wrongAnswer;
+
+// timer
+var timeEl = document.getElementById("timer");
+
+// timer function
+function countdown() {
+  var secondsLeft = 60;
+  var timerInterval = setInterval(function () {
+    if (secondsLeft > 1) {
+      timeEl.textContent = secondsLeft + " seconds left";
+      secondsLeft--;
+    } if (wrongAnswer) {
+      secondsLeft -10;
+      timeEl.textContent = secondsLeft + " seconds left";
+    } else if (secondsLeft === 1) {
+      timeEl.textContent = secondsLeft + " second left";
+    } else {
+      clearInterval(timerInterval);
+    }
+  }, 1000);
+}
+
+countdown();
+
+// questions
