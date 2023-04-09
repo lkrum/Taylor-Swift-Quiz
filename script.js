4e7f04760d722b5f01b3f1cf09397779e03f59a0
lkrum <leahkrumholz73@gmail.com>
2023-04-09 18:10:38 -0500

    added mega array of questions and answers

--- script.js
+++ script.js
@@ -9,30 +9,82 @@
 // WHEN the game is over
 // THEN I can save my initials and score
 
-
 // variables
 var timeEl;
 var answerChoiceEl;
-var question;
+var currentQuestion = {};
 var userAnswer
 var choiceResponse;
+var score = 0
+var questionCounter = 0;
+var questionList = [];
+
+// array of questions and answers
+let questions = [
+  {
+    question: "What is Taylor's lucky number?",
+    choice1: "13",
+    choice2: "3",
+    choice3: "10",
+    choice4: "21",
+    answer: 1
+  },
+  {
+    question: "What was Taylor's first 2020 album?",
+    choice1: "Evermore",
+    choice2: "Folklore",
+    choice3: "Forevermore",
+    choice4: "Woodvale",
+    answer: 2
+  },
+  {
+    question: "Finish the lyric: 'Karma is my ...'",
+    choice1: "god",
+    choice2: "grammy",
+    choice3: "cat",
+    choice4: "boyfriend",
+    answer: 4
+  },
+  {
+    question: "What is the name of Taylor's completely solo-written ablum?",
+    choice1: "Speak Now",
+    choice2: "Fearless",
+    choice3: "Midnights",
+    choice4: "Taylor Swift",
+    answer: 1
+  },
+  {
+    question: "Taylor wrote a short film for which song?",
+    choice1: "Love Story",
+    choice2: "Anti-hero",
+    choice3: "The Man",
+    choice4: "All Too Well (10 Minute Version)",
+    answer: 4
+  },
+
+]
 
 // Question  1 variables
 var questOneOptions = ["13", "10", "3", "21"]
+var question = document.getElementsByTagName("h2");
+console.log(question);
+var correct = ["13", "Folklore", "boyfriend", "Speak Now", "All Too Well (10 Minute Version)"]
+
+// // Game begins
+// var playGame() { 
 
 // Question 1 function
-var questOneChoice = function () {
-  if (questOneChoice.includes("13"))
-    choiceResponse.textContent = "Correct!";
-  else {
-    choiceResponse.textContent = "Wrong!";
+// var questOneChoice = function () {
+//   if (questOneChoice ("13"))
+//     choiceResponse.textContent = "Correct!";
+//   else {
+//     choiceResponse.textContent = "Wrong!";
 
-}
-}
+// }
 
-questOneChoice();
 
-var correct = ["13", "Folklore", "boyfriend", "Speak Now", "All Too Well (10 Minute Version)"]
+
+
 
 // timer
 var timeEl = document.getElementById("timer");
@@ -60,9 +112,11 @@ function countdown() {
 countdown();
 
 // question function
+var questionEl = document.getElementsByClassName("question")
 var answerChoiceEl = document.getElementsByTagName("li");
-var choiceResponse = document.getElementsByClassName("choice-response");
-answerChoiceEl.addEventListener('click', function())
+var choiceResponseEl = Array.from(document.getElementsByClassName("choice-response"));
+console.log(choiceResponseEl)
+// answerChoiceEl.addEventListener('click', function())
 
 //   for (var x = 0; x < answerEl.length; x++)) {
    
@@ -70,9 +124,9 @@ answerChoiceEl.addEventListener('click', function())
   
   
 //   if (userAnswer === ) {
-//       choiceResponse.textContent = "Correct!";
+//       choiceResponseEl.textContent = "Correct!";
 //     } else {
-//       choiceResponse.textContent = "Wrong!";
+//       choiceResponseEl.textContent = "Wrong!";
       
 // }
 // })
